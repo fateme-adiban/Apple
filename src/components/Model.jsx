9b47c66f8f06574e9af4eb8eff2782a1729c7dea
fateme adiban <f.adiban.rad@gmail.com>
2025-12-03 21:18:13 +0330

    fix phone model part

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -61,14 +61,16 @@ const Model = () => {
         </h1>
 
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <ModelView index={1} groupRef={small} gsapType="view1" controlRef={cameraControlSmall} setRotationState={setSmallRotation} item={model} size={size} />
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden! relative">
+            <div className="w-full h-full relative overflow-hidden">
+              <ModelView index={1} groupRef={small} gsapType="view1" controlRef={cameraControlSmall} setRotationState={setSmallRotation} item={model} size={size} />
 
-            <ModelView index={2} groupRef={large} gsapType="view2" controlRef={cameraControlLarge} setRotationState={setLargeRotation} item={model} size={size} />
+              <ModelView index={2} groupRef={large} gsapType="view2" controlRef={cameraControlLarge} setRotationState={setLargeRotation} item={model} size={size} />
 
-            <Canvas className="w-full h-full" style={{ position: "fixed", top: 0, bottom: 0, left: 0, right: 0, overflow: "hidden" }} eventSource={document.getElementById("root")}>
-              <View.Port />
-            </Canvas>
+              <Canvas className="w-full h-full" style={{ position: "fixed", top: 0, bottom: 0, left: 0, right: 0, overflow: "hidden" }} eventSource={document.getElementById("root")}>
+                <View.Port />
+              </Canvas>
+            </div>
           </div>
 
           <div className="mx-auto w-full">
